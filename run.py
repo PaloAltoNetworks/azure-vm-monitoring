c716b717a4356aacf94ccf367c6fa63b53965ad9
Narayan Iyengar <narayan-iyengar@users.noreply.github.com>
2017-06-13 21:47:58 -0700

    Create run.py

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import os
 import json
+import azure
 
-postreqdata = json.loads(open(os.environ['req']).read())
-response = open(os.environ['res'], 'w')
-response.write("hello world from "+postreqdata['name'])
-response.close()
+#postreqdata = json.loads(open(os.environ['req']).read())
+#response = open(os.environ['res'], 'w')
+#response.write("hello world from "+postreqdata['name'])
+#response.close()
+
+print ("hello world\n')
