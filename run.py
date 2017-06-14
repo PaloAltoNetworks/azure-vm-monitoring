87695d011da341a13c348f144c2e41a8f3ce8bce
Narayan Iyengar <niyengar@paloaltonetworks.com>
2017-06-14 13:56:56 -0700

    service principal

--- run.py
+++ run.py
@@ -3,7 +3,11 @@ import json
 import sys
 sys.path.append(os.getcwd()+"\env\Lib\site-packages")
 import azure
+
 from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.resource.resources import ResourceManagementClient
+
+
 subscription_id = "0f3ba96c-a3c7-4eac-b599-ed9882801672"
 credentials = ServicePrincipalCredentials(
     client_id = 'e916a561-bf5f-45d4-b0a8-da65b729e043',
