ea2f292438c74a1d2eda9566b0b83c9230876115
Narayan Iyengar <narayan-iyengar@users.noreply.github.com>
2017-06-14 13:55:14 -0700

    Update run.py

--- run.py
+++ run.py
@@ -4,10 +4,13 @@ import sys
 sys.path.append(os.getcwd()+"\env\Lib\site-packages")
 import azure
 from azure.common.credentials import ServicePrincipalCredentials
+subscription_id = "0f3ba96c-a3c7-4eac-b599-ed9882801672"
+credentials = ServicePrincipalCredentials(
+    client_id = 'e916a561-bf5f-45d4-b0a8-da65b729e043',
+    secret = '8LDuurgo/trTGui9oLxfNEX2RyJ4xtGsA3/M51BnWrU=',
+    tenant = '66b66353-3b76-4e41-9dc3-fee328bd400e'
+)
 
-#postreqdata = json.loads(open(os.environ['req']).read())
-#response = open(os.environ['res'], 'w')
-#response.write("hello world from "+postreqdata['name'])
-#response.close()
+resource_client = ResourceManagementClient(credentials,subscription_id)
 
 print ("hello world\n")
